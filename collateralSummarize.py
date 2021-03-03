3cb13baa7e13559f2f8d5c8f87f0060f2ae0e992
mosaChigira <r.chigira@mosa-architect.com>
2021-03-04 05:39:15 +0900

    合計gmo追加

--- collateralSummarize.py
+++ collateralSummarize.py
@@ -2,6 +2,15 @@ from influxdb import InfluxDBClient
 import pandas as pd
 import time
 
+#　対象取引所　ここだけ編集すればok
+MEASUREMENTS = [
+    'bf_collateral',
+    'bf_SpotCollateral',
+    'bybit_collateral',
+    'gmo_collateral'
+]
+
+
 def getCollateral(measurement):
     res = client.query(f'SELECT {measurement} FROM {measurement} WHERE time>=now() -1m limit 1;')
     getcpu=list(res.get_points(measurement=measurement))
@@ -10,18 +19,13 @@ def getCollateral(measurement):
 
 if __name__ == '__main__':
     client = InfluxDBClient(host='localhost', port=8086, database='bots')
-    measurements = [
-                'bf_collateral',
-                'bf_SpotCollateral',
-                'bybit_collateral'
-            ]
     while True:
         try:
             l = []
-            for measurement in measurements:
+            for measurement in MEASUREMENTS:
                 l.append(getCollateral(measurement))
             data = [{"measurement": "sum_collateral", "fields": {
-              'sum_collateral': sum(l)}}]
+                    'sum_collateral': sum(l)}}]
             client.write_points(data)
         except:
             pass

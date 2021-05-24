69262d6d45d16c2f27a0f0ca23770fa6a0599405
千吉良鯨磨 <r.chigira@mosa-architect.com>
2021-05-24 16:37:06 +0900

    liquid 対応

---
+++ liquidCollateral.py
@@ -0,0 +1,27 @@
+from quoine.client import Quoinex
+import yaml
+import traceback
+import time
+from influxdb import InfluxDBClient
+
+class Liquid(Quoinex):
+    API_URL = 'https://api.liquid.com'
+
+
+if __name__ == '__main__':
+    f = open('key.yaml', 'r+')
+    data = yaml.load(f, Loader=yaml.FullLoader)
+    key = data['liquid_key']
+    secret = data['liquid_secret']
+    liquid = Liquid(key, secret)
+    client = InfluxDBClient(host='localhost', port=8086, database='bots')
+    while True:
+        try:
+            margin = liquid.get_trading_accounts()[0]['equity']
+            data = [{'measurement':"liquid_collateral", "fields":{
+                "liquid_collateral": int(margin)
+            }}]
+            client.write_points(data)
+        except:
+            traceback.print_exc()
+        time.sleep(60)
